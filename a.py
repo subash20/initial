6f38a663e0b8f6390e3a389393b0b93344e2d503
subash20 <medawadisubash@gmail.com>
2019-02-15 14:52:02 +0100

    this is first message

---
+++ a.py
@@ -0,0 +1,15 @@
+#/usr/bin/nev python 
+
+x =input("please enter the number you want")
+
+def num():
+    if (x==9 or x==10):
+        print "true"
+    else:
+        print "False"
+
+
+num()
+#if __name__==__main__():
+ #   main()
+
---
+++ a.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+
+t=$(date +"%Y%m%d_%H%M ")
+
+tar -cvf subash_"$t".tar /home/
